ab536d676c64897174f451a5870f42c463b72324
Brook Li <absreim@github.com>
2021-03-06 23:21:04 -0500

    Set up JWKS client. Removed dependency on socketio-jwt due to type issues and the package not supporting the latest version of socket.io.

--- index.ts
+++ index.ts
@@ -1,17 +1,30 @@
 import { createServer } from 'http';
-import { Server, Socket } from 'socket.io';
+import { Server } from 'socket.io';
+import jwksRsa from 'jwks-rsa';
+
 import stringAssets from './strings.json';
 
 const requiredEnvVars = ['PORT', 'JWKS_URI'];
 
 function checkRequiredEnvVars(requiredEnvVars: string[]) {
 	const missingVars = requiredEnvVars.filter((varName) => !(varName in process.env));
-	if (missingVars.length > 0) {
-		const messageLines = [stringAssets['REQUIRED_ENVIRONMENT_VARIABLES_MISSING']].concat(missingVars);
-		const message = messageLines.join('\n');
-		console.log(message);
-		process.exit(0);
+	if (missingVars.length === 0) {
+		return;
 	}
+	const messageLines = [stringAssets['REQUIRED_ENVIRONMENT_VARIABLES_MISSING']].concat(missingVars);
+	const message = messageLines.join('\n');
+	console.log(message);
+	process.exit(0);
 }
 
 checkRequiredEnvVars(requiredEnvVars);
+
+const port = process.env['PORT']!;
+const jwksUri = process.env['JWKS_URI']!;
+
+const jwksClient = jwksRsa({ jwksUri });
+
+const httpServer = createServer();
+const io = new Server(httpServer);
+
+httpServer.listen(port);
